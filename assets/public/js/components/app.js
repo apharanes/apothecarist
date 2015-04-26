474cb3084e62a8f1807ef7c96ad3f614541a4a0f
Jeanella Klarys Pascual <jeanella@migo.tv>
2015-04-26 15:28:08 +0800

    React: Profiles - init

--- assets/public/js/components/app.js
+++ assets/public/js/components/app.js
@@ -4,26 +4,72 @@ var APP, React;
 React = require('react');
 
 APP = React.createClass({
-  render: function () {
-    return (
-		<div>
-			<h1>Apothecarist</h1>
-			<Diary profile="Matthieu"></Diary>
-		</div>
-	);
-  }
+	getInitialState: function () {
+		return { data: [] };	
+	},
+	render: function () {
+		return (
+			<div>
+				<h1>Apothecarist</h1>
+				<ProfileList profiles="http://localhost:4400/api/profile" />
+			</div>
+		);
+	}
+});
+
+var ProfileList = React.createClass({
+	getInitialState: function () {
+		return { profiles: [] };	
+	},
+	componentDidMount: function () {
+		$.get(this.props.profiles, function (res) {
+			if (this.isMounted()) {
+				this.setState({
+					profiles: res
+				});
+			}
+		}.bind(this));
+	},
+	render: function () {
+		var profiles = this.state.profiles.map(function (profile) {
+			return (
+				<Profile name={profile.name} key={profile._id} />
+			);
+		});
+		
+		return (
+			<div className="profile-list">
+				<h2>Profiles</h2>
+				{profiles}
+			</div>
+		);
+	}
+});
+
+var Profile = React.createClass({
+	render: function () {
+		return (
+			<div className="profile">
+				{this.props.name}
+			</div>
+		);
+	}
 });
 
 var Diary = React.createClass({
+	getInitialState: function () {
+		return { diaryEntries: []};
+	},
 	render: function () {
-	  var diaryEntries = this.props.data.map(function (diaryEntry) {
+		var diaryEntries = this.state.diaryEntries.map(function (diaryEntry) {
 		  return (
 			  <DiaryEntry 
-			  	title={diaryEntry.title}
-			  	date={diaryEntry.date}
+				title={diaryEntry.title}
+				date={diaryEntry.date}
 				/>
-		  )
-	  });
+		  );
+		});
+		
 		return (
 			<div className="diary">
 				<h2>Diary of {this.props.profile}</h2>
